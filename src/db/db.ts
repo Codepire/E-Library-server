71de9fe722b4448f332b73e225467103517e700f
Viral Rupani <viralrupani12017@gmail.com>
2023-10-14 19:28:33 +0530

    feat: host database

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,10 +6,12 @@ config();
 export const AppDataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DATABASE_HOST,
+  url: process.env.DATABASE_URL,
   port: +process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USER,
+  ssl: true,
+  username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  database: 'e-library',
+  database: process.env.DATABASE,
   entities: [Book],
   migrations: ['./dist/migrations/*.js'],
 };
---
+++ src/migrations/1697289959658-initialMigration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1697289959658 implements MigrationInterface {
+    name = 'InitialMigration1697289959658'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "book" ("id" SERIAL NOT NULL, "book_name" character varying NOT NULL, "book_description" character varying NOT NULL, "book_author" character varying NOT NULL, "book_profile" character varying NOT NULL, "book_pdf" character varying NOT NULL, CONSTRAINT "PK_a3afef72ec8f80e6e5c310b28a4" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "book"`);
+    }
+
+}
